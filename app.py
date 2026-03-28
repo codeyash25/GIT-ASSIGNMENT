1bd2e352cc8a63fe9fe59e2ba6bc1faba74a1ed3
YASH <ysakhare418@gmail.com>
2026-03-28 08:39:11 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    name  = 10
+    if name >=10 :
+        name = "Yes"
+    else :
+        name = "No"
+    return render_template('index.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
